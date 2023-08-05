ec7347a4e40ee173bbec075e627b5b43608818b0
Matias Delorenzini <matiasdelorenc@gmail.com>
2023-08-05 14:31:21 -0300

    cart y checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,6 +1,10 @@
 import { useContext, useState } from "react"
 import { CartContext } from "../../context/CartContext"
-import { Timestamp, writeBatch } from "firebase/firestore"
+import { Timestamp, documentId, writeBatch, collection, where, addDoc, getDocs, query } from "firebase/firestore"
+import { db } from "../../services/firebase/firebaseConfig"
+import CheckoutForm from "../CheckoutForm/CheckoutForm"
+
+
 
 const Checkout = () => {
     const [loading, setLoading] = useState(false)
@@ -24,6 +28,62 @@ const Checkout = () => {
             const batch = writeBatch(db)
 
             const outOfStock = []
+
+            const ids = cart.map(prod => prod.id)
+
+            const productsRef = collection(db, 'products')
+
+            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), "in", ids)))
+
+            const { docs } = productsAddedFromFirestore
+
+            docs.forEach(doc => {
+                const dataDoc = doc.data()
+                const stockDb = dataDoc.stock   
+
+                const productAddedtoCart = cart.find(prod => prod.id === doc.id)
+                const prodQuantity = productAddedtoCart?.quantity
+
+                if (stockDb >= prodQuantity) {
+                    batch.update(doc.ref, { stock: stockDb - prodQuantity })
+                } else{
+                    outOfStock.push({ id: doc.id, ...dataDoc})
+                }
+            })
+
+            if(outOfStock.length === 0) {
+                await batch.commit()
+
+                const orderRef = collection(db, "orders")
+
+                const orderAdded = await addDoc(orderRef, objOrder)
+
+                setOrderId(orderAdded.id)
+                clearCart()
+            } else {
+                console.error("hay productos que están fuera de stock")
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
         }
     }
+
+    if(loading) {
+        return <h1>Se está generando su orden...</h1>
+    }
+
+    if(orderId) {
+        return <h1>El id de su orden es : {orderId}</h1>
+    }
+
+    return (
+        <div>
+            <h1>Checkout</h1>
+            <CheckoutForm onconfirm={createOrder}/>
+        </div>
+    )
 }
+
+export default Checkout
