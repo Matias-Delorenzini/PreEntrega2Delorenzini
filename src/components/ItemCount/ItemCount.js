2b81bb6622bafd10453072e8de48ad09ad9d18cd
Matias Delorenzini <matiasdelorenc@gmail.com>
2023-08-09 11:47:43 -0300

    ItemCount mejorado

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,33 +2,62 @@ import React from 'react';
 import { useState } from 'react';
 import "./ItemCount.css"
 
-const ItemCount = ({ stock, initial, onAdd }) => {
-    const [quantity, setQuantity] = useState(initial);
-    const increment = () => {
-        if (quantity < stock) {
-            setQuantity(quantity + 1);
+
+
+function ItemCount({ stock, onAddToCart }) {
+    const [clickCount, setClickCount] = useState(1);
+
+    function handleClickAdd() {
+        if (clickCount === stock) {
+        } else {
+            setClickCount(clickCount + 1);
         }
-    };
-    const decrement = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
+    }
+
+    function handleClickSub() {
+        if (clickCount > 1) {
+            setClickCount(clickCount - 1);
         }
-    };
+    }
 
     return (
-    <div className="Counter">
-        <div className="Controls">
-            <button className="Button" onClick={decrement}>-</button>
-            <h4 className="Number">{quantity}</h4>
-            <button className="Button" onClick={increment}>+</button>
-        </div>
         <div>
-            <button className="Button" onClick={() => onAdd(quantity)} disabled={stock === 0}>
-                Añadir al carrito
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+                className="item-count-container"
+            >
+                <button
+                    disabled={stock === 0}
+                    className="item-count-button"
+                    onClick={handleClickSub}
+                >
+                    -
+                </button>
+                <h2 className="count-text">{clickCount}</h2>
+                <button
+                    disabled={stock === 0}
+                    className="item-count-button"
+                    onClick={handleClickAdd}
+                >
+                    +
+                </button>
+            </div>
+
+            <button
+                onClick={() => onAddToCart(clickCount)} 
+                className="item-count-button add-to-cart-button"
+            >
+                <h4>Añadir al carrito</h4>
             </button>
         </div>
-    </div>
     );
-};
+}
 
 export default ItemCount;
+
+
